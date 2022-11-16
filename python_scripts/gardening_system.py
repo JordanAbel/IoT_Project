eb117462a9ed66b1d5c691255ed6b45347643941
Yajat Kumar <yajatapps3@gmail.com>
2022-11-16 09:42:15 -0800

    Send data to moisture and brightness feeds

--- python_scripts/gardening_system.py
+++ python_scripts/gardening_system.py
@@ -1,4 +1,4 @@
-#Libraries
+# Libraries
 import RPi.GPIO as GPIO
 import time
 import serial
@@ -16,24 +16,18 @@ ADAFRUIT_IO_USERNAME = 'curtishallman'
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-
-
-def sendFeed (str, str2):
-    aio.send(temperature.key, temperature_c)
-    aio.send(humidity.key, humidity_value)
-    
-try: # if we have a 'temperature' feed
+try:  # if we have a 'temperature' feed
     temperature = aio.feeds('temperature')
-except RequestError: # create a temperature feed
+except RequestError:  # create a temperature feed
     feed = Feed(name="temperature")
     temperature = aio.create_feed(feed)
-    
-try: # if we have a 'humidity' feed
+
+try:  # if we have a 'humidity' feed
     humidity = aio.feeds('humidity')
-except RequestError: # create a humidity feed
+except RequestError:  # create a humidity feed
     feed2 = Feed(name="humidity")
     humidity = aio.create_feed(feed2)
-    
+
 try: # if we have a 'brightness' feed
     brightness = aio.feeds('brightness')
 except RequestError: # create a brightness feed
@@ -45,48 +39,47 @@ try: # if we have a 'moisture' feed
 except RequestError: # create a moisture feed
     feed4 = Feed(name="moisture")
     moisture = aio.create_feed(feed4)
-    
-#GPIO Mode (BOARD / BCM)
-dht_sensor = adafruit_dht.DHT11(board.D4,use_pulseio=False)
 
-BG_PIN =  17
+# GPIO Mode (BOARD / BCM)
+dht_sensor = adafruit_dht.DHT11(board.D4, use_pulseio=False)
+
+BG_PIN = 17
 GPIO.setup(BG_PIN, GPIO.IN)
 
 ser = serial.Serial('/dev/ttyACM1', 9600)
 time.sleep(1)
-#send data to dashboard
-while True:
+
+
+def sendFeed(str1, str2, str3, str4):
+    aio.send(temperature.key, str1)
+    aio.send(humidity.key, str2)
+    aio.send(brightness.key, str3)
+    aio.send(moisture.key, str4)
+
+
+def loop():
     try:
         temperature_c = dht_sensor.temperature
         humidity_value = dht_sensor.humidity
-    
+
         print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature_c, humidity_value))
-        
+
         print(GPIO.input(BG_PIN))
-        
-        sendFeed(temperature_c, humidity_value)
-    except:
-        a = "Sensor failure. Check wiring.";
-    
-    time.sleep(5);
-    
-    
-#ser = serial.Serial('/dev/ttyACM1', 9600)
-#time.sleep(1)
 
-while True:
-    line = ser.readline();
-
-    try:
+        line = ser.readline()
         msg = line.decode()
         values = msg.split(":")
 
         brightness_value = values[0]
         moisture_value = values[1]
 
-        print("Brightness: " + brightness_value)
-        print("Moisture: " + moisture_value)
-        
-
+        sendFeed(temperature_c, humidity_value, brightness_value, moisture_value)
     except:
         pass
+
+    time.sleep(5)
+
+
+# send data to dashboard
+while True:
+    loop()
